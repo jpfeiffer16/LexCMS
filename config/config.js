cf74bae49f6eb95ac40ed82b01e08897a77e302d
jpfeiffer16 <jpfeiffer@bluemodus.com>
2015-11-03 11:07:07 -0700

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'lexcms'
+    },
+    port: 3000,
+    db: 'mongodb://admin:admin@ds049084.mongolab.com:49084/lexcms'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'lexcms'
+    },
+    port: 3000,
+    db: 'mongodb://admin:admin@ds049084.mongolab.com:49084/lexcms'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'lexcms'
+    },
+    port: 3000,
+    db: 'mongodb://admin:admin@ds049084.mongolab.com:49084/lexcms'
+  }
+};
+
+module.exports = config[env];
