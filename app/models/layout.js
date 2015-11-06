58c17a4cbc3fbff92478338f0f8d69cfde0c3827
Joe Pfeiffer <jpfeiffer@bluemodus.com>
2015-11-06 07:44:53 -0700

    Basic work on layout providers

---
+++ app/models/layout.js
@@ -0,0 +1,17 @@
+/// <reference path="../../typings/mongoose/mongoose.d.ts" />
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var LayoutShcema = new Schema({
+  _id: mongoose.Types.ObjectId,
+  title: String,
+  code: String
+});
+
+LayoutShcema.virtual('date')
+  .get(function(){
+    return this._id.getTimestamp();
+  });
+
+module.exports = mongoose.model('Layout', LayoutShcema);
+
---
+++ app/modules/LayoutProvider.js
@@ -0,0 +1,60 @@
+/// <reference path="../../typings/mongoose/mongoose.d.ts" />
+
+
+var Layout = require('../models/layout.js'),
+	mongoose = require('mongoose');
+
+
+module.exports = (function() {
+	
+	function getLayout(id, callback) {
+		
+		var onFinished = function() {return};
+		
+		if (typeof(callback) == 'function') {
+			onFinished = callback;
+		}
+		
+		
+		Layout.findOne({ _id: id }, function(err, document) {
+			if (!err) {
+				onFinished(err, document);
+			} else {
+				onFinished(err, null);
+			}
+		});
+		
+		
+	}
+	
+	
+	
+	function saveLayout(title, code, callback) {
+		
+		
+		var id = new mongoose.Types.ObjectId();
+		
+		
+		var newLayout = new Layout({
+			_id: id,
+			title: title,
+			code: code
+		});
+		newLayout.save(function(err) {
+			if (!err) {
+				if (typeof(callback) == 'function') {
+					callback(err, id);
+				} else {
+					callback(err, null);
+				}
+			}
+		});
+	}
+	
+	
+	
+	
+	return {
+		getLayout: getLayout
+	};
+})();
